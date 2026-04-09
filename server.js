75bd9beb72a045b1db3ee04de0e7af036b34f722
Marcos Vinny <vinnymarcos2006@gmail.com>
2026-04-09 11:15:28 -0300

    preparando para o render

--- server.js
+++ server.js
@@ -54,6 +54,5 @@ app.delete("/chamado/:id", async (req, res) => {
   res.send({ ok: true });
 });
 
-app.listen(3000, () =>
-  console.log("🚀 Servidor rodando em http://localhost:3000")
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
